589f25bb92e8ccf5c48b619266dfc9c0d034f7bb
HeberteA <heberte.2307@gmail.com>
2025-08-30 16:01:44 -0300

    Update main.py

--- main.py
+++ main.py
@@ -993,8 +993,10 @@ else:
                     aviso_atual = obras_df.loc[obras_df['NOME DA OBRA'] == obra_selecionada, 'Aviso'].iloc[0]
                 
                 novo_aviso = st.text_area(
+                    "Edite o aviso",
                     value=aviso_atual, 
-                    key=f"aviso_{obra_selecionada}"
+                    key=f"aviso_{obra_selecionada}
+                    label_visibility="collapsed"
                     
                 if st.button("Salvar Aviso", key=f"btn_aviso_{obra_selecionada}"):
                     obras_df = save_aviso_data(obras_df, obra_selecionada, novo_aviso)
@@ -1100,6 +1102,7 @@ else:
                                         st.rerun()
                                     except Exception as e:
                                         st.error(f"Ocorreu um erro ao salvar as observações: {e}")
+
 
 
 
