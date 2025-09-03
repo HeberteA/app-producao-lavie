7442203e4a57273b39ae9aec8a4d924fb9126d43
HeberteA <heberte.2307@gmail.com>
2025-09-03 16:33:43 -0300

    Update main.py

--- main.py
+++ main.py
@@ -30,8 +30,14 @@ def load_data(_engine):
 
     # Query para Funcionários (com obra_id)
     query_funcionarios = """
-    SELECT f.id, f.obra_id, f.nome as "NOME", o.nome_obra as "OBRA", 
-           fn.funcao as "FUNÇÃO", fn.tipo as "TIPO", fn.salario_base as "SALARIO_BASE"
+    SELECT 
+        f.id,
+        f.obra_id, -- ADICIONE ESTA LINHA
+        f.nome as "NOME",
+        o.nome_obra as "OBRA",
+        fn.funcao as "FUNÇÃO",
+        fn.tipo as "TIPO",
+        fn.salario_base as "SALARIO_BASE"
     FROM funcionarios f
     JOIN obras o ON f.obra_id = o.id
     JOIN funcoes fn ON f.funcao_id = fn.id;
@@ -1288,6 +1294,7 @@ else:
                                         st.toast("Observações salvas com sucesso!", icon="✅")
                                         st.cache_data.clear()
                                         st.rerun()
+
 
 
 
