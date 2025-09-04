e3121bd6fade3a39df05e5556a1b16d96e07d536
HeberteA <heberte.2307@gmail.com>
2025-09-04 15:15:42 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1280,7 +1280,7 @@ else:
             lancamentos_obra_df = lancamentos_df[lancamentos_df['Obra'] == obra_selecionada]
             funcionarios_obra_df = funcionarios_df[funcionarios_df['OBRA'] == obra_selecionada]
 
-            status_geral_row = status_df[(status_df['Obra'] == obra_selecionada) & (status_df['Funcionario'] == 'Status Geral da Obra') & (status_df['Mes'] == mes_selecionado_dt)]
+            status_geral_row = status_df[(status_df['Obra'] == obra_selecionada) & (status_df['Obra'] == 'Status Geral da Obra') & (status_df['Mes'] == mes_selecionado_dt)]
             status_atual_obra = status_geral_row['Status'].iloc[0] if not status_geral_row.empty else "A Revisar"
             
             folha_lancada_row = folhas_df[(folhas_df['Obra'] == obra_selecionada) & (folhas_df['Mes'] == mes_selecionado_dt)] 
@@ -1305,7 +1305,7 @@ else:
                     if len(nomes_funcionarios_obra) > 0:
                         status_funcionarios_obra = status_df[(status_df['Obra'] == obra_selecionada) & (status_df['Mes'] == mes_selecionado)]
                         for nome in nomes_funcionarios_obra:
-                            status_func_row = status_df[(status_df['Obra'] == obra_selecionada) & (status_df['Funcionario'] == Status Geral da Obra) & (status_df['Mes'] == mes_selecionado_dt)]
+                            status_func_row = status_funcionarios_obra[status_funcionarios_obra['Funcionario'] == nome]
                             status_atual_func = status_func_row['Status'].iloc[0] if not status_func_row.empty else "A Revisar"
                             if status_func != 'Aprovado':
                                 todos_aprovados = False
@@ -1472,6 +1472,7 @@ else:
                                         st.toast("Observações salvas com sucesso!", icon="✅")
                                         st.cache_data.clear()
                                         st.rerun()
+
 
 
 
