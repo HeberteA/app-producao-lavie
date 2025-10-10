3874f142ca19ca1563565fe7adb4f0da0dbac636
HeberteA <heberte.2307@gmail.com>
2025-10-10 10:48:06 -0300

    Update resumo_da_folha.py

--- paginas/resumo_da_folha.py
+++ paginas/resumo_da_folha.py
@@ -4,7 +4,6 @@ import db_utils
 import utils
 
 def render_page(engine):
-    engine = db_utils.get_db_connection()
     if engine is None:
         st.error("Falha na conexão com o banco de dados. A página não pode ser carregada.")
         st.stop()
@@ -82,6 +81,7 @@ def render_page(engine):
             ),
             use_container_width=True
         )
+
 
 
 
