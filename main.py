029cac23ffee0082dc9ea8c945690b4caa1c7bdb
HeberteA <heberte.2307@gmail.com>
2025-10-13 09:33:58 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 import streamlit as st
 from datetime import datetime, timedelta, date
 import io
+import sys
 import pandas as pd
 import db_utils
 import utils
@@ -179,6 +180,7 @@ else:
     
     if page_to_render in page_map:
         page_map[page_to_render].render_page()
+
 
 
 
