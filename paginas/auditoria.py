e64d5bc187faef0eb900aa4b81d4d69dd015a704
HeberteA <heberte.2307@gmail.com>
2025-11-20 10:51:30 -0300

    Update auditoria.py

--- paginas/auditoria.py
+++ paginas/auditoria.py
@@ -19,7 +19,7 @@ def render_page():
     }
     .audit-stat-container:hover {
         background-color: rgba(255, 255, 255, 0.1);
-        weight: 100;
+        weight: 150;
     }
     .audit-stat-label {
         font-size: 0.7rem;
